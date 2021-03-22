5793de8b4036da9d50a47545cdd177f4cc3da652
martinweiser <martin.weiser@gmail.com>
2021-03-22 23:50:45 +0100

    simple html5 vanillaJS form fun

--- sharedApp/src/components/CallOut.js
+++ sharedApp/src/components/CallOut.js
@@ -2,13 +2,29 @@
 import './callout.scss'
 import AtomComponent from './AtomComponent'
 
-const template = ({ title, text, buttonLabel = 'Close', buttonClick }) => `<div class="callout callout-content">
+const template = function _template({ title, text, buttonLabel = 'Close', buttonClick }) {
+  _template.buttonClick = buttonClick
+  console.log('this', this)
+  console.log('_template', _template)
+  return `
+    <div class="callout callout-content">
+      <h2>${title}</h2>
+      <p>${text}</p>
+      <button onclick="_template.buttonClick(); return false;">${buttonLabel}</button>
 
-    <h2>${title}</h2>
-    <p>${text}</p>
-    <button onclick="${buttonClick}">${buttonLabel}</button>
 
-</div>
-`
+      <form id="myForm" onsubmit="myFormSubmit()">
+        <label>Email:<input id="email" type="email"></label>
+        <label>Password:<input name="password" type="password"></label>
+      </form>
+    </div>
+  `
+}
 
-export const CallOut = new AtomComponent({ template })
+export const CallOut = new AtomComponent({
+  template,
+  buttonClick: () => {
+    alert('Call Out')
+    console.log('buttonClick, this', this)
+  },
+})
--- sharedApp/src/index.js
+++ sharedApp/src/index.js
@@ -7,3 +7,24 @@ app.render()
 
 
 CallOut.render({ id: 'callout', title: 'Call Out', text: 'dsdsds dsadd ada sadsa sadsa dsdsad asdsa dsd sdsad sdsa' })
+
+
+const formElement = document.getElementById('myForm')
+
+formElement.addEventListener('input', (event) => {
+  console.log('myForm input', event)
+  console.log(formElement)
+  console.log(formElement.elements)
+  console.log(Array.prototype.map.apply(formElement.elements, [(item) => item.value]))
+  console.log('formElement.email', formElement.email)
+  console.log('formElement.password', formElement.password)
+})
+
+formElement.addEventListener('change', (event) => {
+  console.log('myForm change', event)
+  console.log(formElement)
+  console.log(formElement.elements)
+  console.log(Array.prototype.map.apply(formElement.elements, [(item) => item.value]))
+  console.log('formElement.email', formElement.email)
+  console.log('formElement.password', formElement.password)
+})
